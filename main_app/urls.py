f4107f99fc701fa25ee52609cc138bb3f868f222
scott95100 <scott95100@gmail.com>
2021-03-19 16:55:12 -0700

    all cats displaying from the db

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cats/', views.cats_index, name='cats')
 ]
